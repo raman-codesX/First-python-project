d6d6535f127db3d6401a136df3438e953d9f545d
Raman <namansingh982150@gmail.com>
2025-06-25 09:59:05 +0530

    first.py

--- game.py
+++ game.py
@@ -1,12 +1,9 @@
+#Age calculator
 
-# name = input("enter your name :")
-# dob = int(input("enter your birth year:"))
+name = input("enter your name :")
+dob = int(input("enter your birth year:"))
 
-# age = 2025 - dob
+age = 2025 - dob
 
-# print("your current age is :", age)
+print("your current age is :", age)
 
-name = input("enter your data :")
-leng = name.count("$")
-
-print(leng)
