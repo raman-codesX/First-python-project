a7a192b12ac451731619d34f43f699d50611c172
Raman <namansingh982150@gmail.com>
2025-06-25 09:53:31 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+
+# name = input("enter your name :")
+# dob = int(input("enter your birth year:"))
+
+# age = 2025 - dob
+
+# print("your current age is :", age)
+
+name = input("enter your data :")
+leng = name.count("$")
+
+print(leng)
