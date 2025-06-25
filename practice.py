e02ca42416d2934c0ab0b38bd9a43bc18744b99a
Raman <namansingh982150@gmail.com>
2025-06-25 10:07:29 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,33 @@
+# marks = int(input("enter your marks :"))
+
+# if(marks >= 80):
+#     print("A")
+# elif(marks>=75):
+#     print("B")
+# elif(marks>=60):
+#     print("C")
+# elif(marks>=50):
+#     print("D")
+# else:
+#     print("You are fail")
+
+
+# num = int(input("enter a number :"))
+
+# if(num%2 != 0):
+#     print("Number is ==> odd")
+# else:
+#     print("Number is ==> even")
+
+
+
+# 4
+
+
+
+num = int(input("enter a number :"))
+
+if(num%7 == 0):
+    print("Number is multipled by 7")
+else:
+    print("Number is multipled not by 7")
